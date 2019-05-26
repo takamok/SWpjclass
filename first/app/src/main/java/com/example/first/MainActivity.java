36f43ef73efd3e22722c599c3b27627693436291
costick8285 <whgksrb28@gmail.com>
2019-05-26 21:31:08 +0900

    지도 연동

---
+++ first/app/src/main/java/com/example/first/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.first;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import net.daum.mf.map.api.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+
+    }
+}
