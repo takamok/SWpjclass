20f505ec58d4a45a6be8851a99923e173e6f9f42
kangchanghee <https://github.com/takamok/SWpjclass.git>
2019-06-12 03:54:01 +0900

    코드

---
+++ 6.12/app/src/main/java/com/example/newapp/DbOpenHelper.java
@@ -0,0 +1,44 @@
+package com.example.newapp;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbOpenHelper {
+
+    private static final String DATABASE_NAME = "NewApp(SQLite).db";
+    private static final int DATABASE_VERSION = 1;
+    public static SQLiteDatabase mDB;
+    private DatabaseHelper mDBHelper;
+    private Context mCtx;
+
+    private class DatabaseHelper extends SQLiteOpenHelper {
+        public DatabaseHelper(Context context, String name, CursorFactory factory, int version) {
+            super(context, name, factory, version);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(DataBases.CreateDB._CREATE0);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + DataBases.CreateDB._TABLENAME0);
+            onCreate(db);
+        }
+    }
+
+    public DbOpenHelper(Context context) {
+        this.mCtx = context;
+    }
+
+    public DbOpenHelper open() throws SQLException {
+        mDBHelper = new DatabaseHelper(mCtx, DATABASE_NAME, null, DATABASE_VERSION);
+        mDB = mDBHelper.getWritableDatabase();
+        return this;
+    }
+}
--- 6.12/app/src/main/java/com/example/newapp/MainActivity.java
+++ 6.12/app/src/main/java/com/example/newapp/MainActivity.java
@@ -1,15 +1,96 @@
 package com.example.newapp;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import net.daum.mf.map.api.MapView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private static int PICK_IMAGE_REQUEST = 1;
+    public TextView mView, metadata,mTimestamp;
+    Button Gallary;
+    Button Review;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTimestamp=(TextView)findViewById(R.id.Timestamp);
+        mView = (TextView) findViewById(R.id.metadata);
+        metadata = (TextView) findViewById(R.id.metadatatwo);
+
+        mTimestamp = (TextView) findViewById(R.id.Timestamp);
+        mView = (TextView) findViewById(R.id.metadata);
+        metadata = (TextView) findViewById(R.id.metadatatwo);
+
+        MapView mapView = new MapView(this);
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+        Gallary = findViewById(R.id.Gallary);
+        Review = findViewById(R.id.Review);
+        Review.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ReviewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Gallary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
+                //Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
+                i.setType("image/*"); //이미지만 보이게
+                //Intent 시작 - 갤러리앱을 열어서 원하는 이미지를 선택할 수 있다.
+                startActivityForResult(i, 1);
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && null != data) {
+                Uri uri = data.getData();
+                Log.d("Real path is : ", getRealPath(data.getData()));
+            } else {
+                Toast.makeText(this, "onActivity취소 되었습니다.", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Oops! onActivity로딩에 오류가 있습니다.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+    public String getRealPath(Uri uri) {
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor c = getContentResolver().query(uri, proj, null, null, null);
+        int index = c.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        c.moveToFirst();
+        String path = c.getString(index);
+        try {
+            ExifInterface exif = new ExifInterface(path);
+            //호출
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Error!", Toast.LENGTH_LONG).show();
+        }
+        return path;
     }
 }
